9998067f6b8d499b92a41a198fa2d682a22ce3a4
unknown <rafaeldasilvaperes@hotmail.com>
2022-01-02 17:21:47 -0300

    Starting the repo for the project Scrapity Scraper

---
+++ Dream Price/scrapity_scraper.py
@@ -0,0 +1,66 @@
+import requests
+from bs4 import BeautifulSoup
+import smtplib
+import os
+import time
+
+# Asus Dual NVIDIA GeForce GTX 1650 4GB, GDDR5 - DUAL-GTX1650-4G
+URL_grapicCard = 'https://bit.ly/3qFgDUb'
+# Forno de embutir elétrico Consul 84 litros prata com Grill e Timer Autodesligamento - COB84AR
+URL_consulOven = 'https://bit.ly/34fnuvW'
+  
+headers = {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36'}
+
+def cp_grapic_card():
+  page = requests.get(URL_grapicCard, headers=headers)
+  soup = BeautifulSoup(page.content, 'html.parser')
+  body_message = "Damn, can you even run Pecman on this machine bro?! Too weak! Please, check this out: "
+
+  price = soup.find("h4", itemprop="price").get_text()
+  print(price)
+  converted_price = float(price[3:8])
+
+  print(converted_price) # pass this information to a db
+
+  if(converted_price < 1.800):
+    send_mail(URL_grapicCard, body_message)
+
+def cp_consul_oven():
+  page = requests.get(URL_consulOven, headers=headers)
+  soup = BeautifulSoup(page.content, 'html.parser')
+  body_message = "Is your kitchen ready to look badass?! Well, so check this link: "
+  
+  price = soup.find("span", "vtex-product-price-1-x-spotPrice").get_text()
+  print(price)
+  converted_price = float(price[3:8])
+
+  print(converted_price) # pass this information to a db
+
+  if(converted_price < 1.400):
+    send_mail(URL_consulOven, body_message)
+
+def send_mail(url, body_msg):
+  server = smtplib.SMTP('smtp-mail.outlook.com', 587)
+  server.ehlo()
+  server.starttls()
+  server.ehlo()
+
+  server.login(os.environ.get('EMAIL'), os.environ.get('PW'))
+  subject = 'The Manager Finally Went Nuts!'
+  body = f'{body_msg} {url}'
+  to_addr = os.environ.get("EMAIL")
+  from_addr = "Rafaelo <" + os.environ.get("EMAIL") + ">"
+
+  msg = "From: %s\nTo: %s\nSubject: %s\n\n %s" %(from_addr, to_addr, subject, body)
+
+  server.sendmail(from_addr, to_addr, msg )
+
+  print("HEY EMAIL HAS BEEN SENT!")
+
+  server.quit()
+
+while(True):
+  cp_consul_oven()
+  cp_grapic_card()
+  time.sleep(86400)
+
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# Scrapity_Scraper
-A simple project with a wannabe web-scraping script on Python!
+# Scrapity Scraper!
+
+[![Project Status: WIP – Initial development is in progress, but there has not yet been a stable, usable release suitable for the public.](https://www.repostatus.org/badges/latest/wip.svg)](https://www.repostatus.org/#wip)
+
+## What's been done so far:
+
+<p align="justify">Here I created a script on Python to basically keep digging the price of two items from one single website each 4 times a day and keep checking if the price is above the value I setted as the "Dream Price", and if so it will send me an e-mail warning that I can now tell someone whose wallet is fuller than mine to buy these products right away!</p>
+
+## What will be done next:
+
+<p align="justify">I intend to create a database that will store important informations like date/time/price and build a simple web page that keeps updating and showing this online. Will probably be deployed on Heroku.</p>
+
+
